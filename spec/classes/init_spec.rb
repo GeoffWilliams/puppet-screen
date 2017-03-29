db307f40f8c12451b4087b55f6e3af9462bab74b
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-03-29 14:39:03 +1100

    test on windows but only a little bit https://github.com/rodjek/rspec-puppet/issues/192

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,13 +1,41 @@
 require 'spec_helper'
 describe 'screen' do
-  let :facts do
-    {
-      :os => {
-        'family' => 'Suse'
+
+  context 'linux' do
+    let :facts do
+      {
+        :os => {
+          'family' => 'Suse'
+        }
       }
-    }
+    end
+    context 'with default values for all parameters' do
+      it { should contain_class('screen') }
+    end
+
+    context 'compiles' do
+      it { should compile }
+    end
   end
-  context 'with default values for all parameters' do
-    it { should contain_class('screen') }
+
+  context 'windows' do
+    let :facts do
+      {
+        :os => {
+          'family' => 'windows'
+        }
+      }
+    end
+    context 'with default values for all parameters' do
+      it { should contain_class('screen') }
+    end
+
+    # can't test file paths on windows:
+    #   https://tickets.puppetlabs.com/browse/MODULES-4287
+    #   https://github.com/rodjek/rspec-puppet/issues/192
+    #   https://github.com/rodjek/rspec-puppet/issues/437
+    # context 'compiles' do
+    #   it { should compile }
+    # end
   end
 end
